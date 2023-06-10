bf05aac4b6f5312f6657146ce5c18cc74f816df4
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-10 22:00:36 +0400

    Update python.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- python.py
+++ python.py
@@ -19,12 +19,12 @@ session = requests.Session()
 
 def create_table(conn):
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS processed_jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, job_link TEXT UNIQUE, post_date TEXT, job_response TEXT, title_text TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS processed_jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, job_link TEXT, post_date TEXT, job_response TEXT, title_text TEXT)")
 
 
 def save_processed_job(conn, job_link, post_date, job_response, title_text):
     cursor = conn.cursor()
-    cursor.execute("INSERT OR IGNORE INTO processed_jobs (job_link, post_date,job_response, title_text) VALUES (?, ?,?, ?)", (job_link, post_date, job_response, title_text))
+    cursor.execute("INSERT INTO processed_jobs (job_link, post_date, job_response, title_text) VALUES (?, ?, ?, ?)", (job_link, post_date, job_response, title_text))
     conn.commit()
     send_notification(f"{post_date},{job_response}\n\n{job_link}")
 
@@ -32,18 +32,21 @@ def save_processed_job(conn, job_link, post_date, job_response, title_text):
 def load_processed_jobs():
     conn = sqlite3.connect('processed_jobs_free.db')
     cursor = conn.cursor()
-    sql = "SELECT id, job_link, post_date, job_response, title_text FROM processed_jobs ORDER BY id DESC LIMIT 50"
+    sql = "SELECT job_link FROM processed_jobs"
     sql_exec = cursor.execute(sql).fetchall()
+  
     processed_jobs = []
     for row in sql_exec:
-        job = {
-            "id": row[0],
-            "job_link": row[1],
-            "post_date": row[2],
-            "job_response": row[3],
-            "title_text": row[4]
-        }
-        processed_jobs.append(job)
+        processed_jobs.append(row[0])
+    # for row in sql_exec:
+    #     job = {
+    #         "id": row[0],
+    #         "job_link": row[1],
+    #         "post_date": row[2],
+    #         "job_response": row[3],
+    #         "title_text": row[4]
+    #     }
+    #     processed_jobs.append(job)
     return processed_jobs
 
 
@@ -80,15 +83,10 @@ def extract_post_date(item):
 
 
 while True:
-    
+    processed_jobs = load_processed_jobs()
     try:
         # search_query = input("Enter the word to search: ").lower()  # Accept user input
-        full_link = []  
-        processed_jobs = load_processed_jobs()
-        for dict_ in processed_jobs:
-            full_link.append(dict_['job_link'])
-
-        for page in range(1, 50):
+        for page in range(1, 10):
             print(page)
             url = f"https://www.weblancer.net/jobs/?page={page}"
             html = session.get(url, headers=headers).text
@@ -96,31 +94,27 @@ while True:
             job_items = soup.find_all(class_="row click_container-link set_href")
         
             for item in job_items:
-                job_link = item.find("div", class_="title").find("a")["href"]
+ 
+                job_link = item.find("div", class_="title").find("a")["href"].strip()
                 job_response = item.find(class_="col-sm-8 text-muted dot_divided text_field d-sm-flex").text.strip() if item.find(class_="col-sm-8 text-muted dot_divided text_field d-sm-flex").text is not None else "нет категории"
                 
                 if job_link is not None:
                     title_text = item.find("div", class_="title").text.lower().strip() if item.find("div", class_="title").text.lower() else ""
                     description_element = item.find("div", class_="collapse").text if item.find("div", class_="collapse") else ""
-             
-                    # print(title_text)
-                    favorite_task = ["парсер","парсинг","спарсить", "python", "пайтон", "питон", "flask", "django", "fastapi"]
-                    # if any(word in title_text for word in favorite_task):
-                    if (title_text in favorite_task) or (description_element in favorite_task):
-                        job_link = "https://www.weblancer.net/"+job_link
+                    hash_tag = [i.text.strip() for i in item.find(class_="col-sm-8 text-muted dot_divided text_field d-sm-flex").find(class_="flex-wrap")] if item.find(class_="col-sm-8 text-muted dot_divided text_field d-sm-flex").find(class_="flex-wrap") else ""
+                    favorite_task = ["парсер","парсинг","спарсить", "python", "пайтон", "питон", "flask", "django", "fastapi", "php", "js", "пхп", "javascript", "джанго", "фласк","фастапи"]
+
+                    if any(word in title_text or word in description_element or word in hash_tag for word in favorite_task):
+                        job_link_save = f"https://www.weblancer.net{job_link}"
                         post_date, title_time = extract_post_date(item)
-                        # print(title_time)
-                        
-                        if job_link not in full_link:
-                            save_processed_job(conn, job_link, title_time, job_response, title_text)
-                         
+                        if job_link_save not in processed_jobs:
+                            save_processed_job(conn, job_link_save, title_time, job_response, title_text)    
             time.sleep(1)
         time.sleep(60)
 
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         traceback.print_exc()
-        # Задержка в 10 минут перед повторной попыткой
 conn.close()
 
 
